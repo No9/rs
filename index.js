59d49dc846ec0f8e318cd7778ac672b5aa2a6534
Anton Whalley <anton.whalley@hmhco.com>
2015-08-18 01:24:39 +0100

    sensible lib name in sample2

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var ffi = require('ffi');
 var path = require('path')
 var assert = require('assert')
 console.log(path.join(__dirname, 'target/debug/rscalc'))
-var lib = ffi.Library(path.join(__dirname, 'target/debug/rscalc'), {  
+var rscalc = ffi.Library(path.join(__dirname, 'target/debug/rscalc'), {  
   fibonacci: ['int', ['int']]
 });
 
-lib.fibonacci.async(20, function (err, res) {
+rscalc.fibonacci.async(20, function (err, res) {
   if (err) throw err
   assert(6765, res)
   console.log("mycall returned " + res)
